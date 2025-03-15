f5e80969de9540b8075dcd90f8d518b263f7d5c6
MarvinVN <marvinvnguyen@gmail.com>
2025-03-14 20:53:45 -0400

    impl create-trip-card, reverse trip display order

--- frontend/src/app/trips/components/create-trip-card.tsx
+++ frontend/src/app/trips/components/create-trip-card.tsx
@@ -1,36 +1,84 @@
-import { Trip } from "@/app/types";
-import { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from '@/components/ui/card'
+import React from 'react';
+import axios from 'axios';
+import { useAuth } from '../../context/AuthContext';
+import { DateRange } from "react-day-picker";
+import { Card, CardTitle } from '@/components/ui/card';
 import {
     Dialog,
-    DialogClose,
     DialogContent,
     DialogDescription,
     DialogFooter,
     DialogHeader,
-    DialogOverlay,
-    DialogPortal,
     DialogTitle,
     DialogTrigger,
-  } from '@/components/ui/dialog'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { IconPlus } from '@tabler/icons-react'
+} from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { IconPlus } from '@tabler/icons-react';
+import { DatePickerWithRange } from "@/components/ui/date-range-picker";
 
-export function CreateTripCard() {
+interface CreateTripCardProps {
+  onTripCreated: () => void;
+}
 
-    return (
-        <Dialog>
-            <DialogTrigger asChild>
-            <Card className="place-content-end bg-gray-300 hover:bg-gray-400">
-                <div className="flex justify-center">
-                    <IconPlus size={70}/>
-                </div>
-                <div className="flex justify-center">
-                    <CardTitle>Create a new trip</CardTitle>
-                </div>
-            </Card>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
+export function CreateTripCard({ onTripCreated }: CreateTripCardProps) {
+
+  const [title, setTitle] = React.useState('');
+  const [description, setDescription] = React.useState('');
+  const [startDate, setStartDate] = React.useState<Date | undefined>(undefined)
+  const [endDate, setEndDate] = React.useState<Date | undefined>(undefined) 
+  const [open, setOpen] = React.useState(false)
+  const { getToken } = useAuth();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const token = await getToken()
+    try {
+        const headers = {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        }
+        const { data } = await axios.post('http://localhost:8000/api/trips',
+          { 
+            "title": title,
+            "description": description,
+            "start_date": startDate,
+            "end_date": endDate
+          },
+          { headers });
+
+        console.log(data)
+        
+        setTitle('');
+        setDescription('');
+        setStartDate(undefined);
+        setEndDate(undefined);
+        handleDateChange(undefined);
+        setOpen(false);
+        onTripCreated();
+    } catch (error) {
+        console.error(error);
+    }
+  }
+
+  const handleDateChange = (date: DateRange | undefined) => {
+    setStartDate(date?.from);
+    setEndDate(date?.to)
+  }
+
+  return (
+      <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+          <Card className="place-content-end bg-gray-300 hover:bg-gray-400">
+              <div className="flex justify-center">
+                  <IconPlus size={70}/>
+              </div>
+              <div className="flex justify-center">
+                  <CardTitle>Create a new trip</CardTitle>
+              </div>
+          </Card>
+          </DialogTrigger>
+      <DialogContent className="w-xl">
         <DialogHeader>
           <DialogTitle>Create a Trip</DialogTitle>
           <DialogDescription>
@@ -38,33 +86,44 @@ export function CreateTripCard() {
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <h1 className="text-right">
+          <div className="space-y-2">
+            <h1 className="after:content-['*'] after:ml-0.5 after:text-red-500">
               Trip Title
             </h1>
             <Input
               id="trip_title"
               defaultValue="Name your trip"
               className="col-span-3 text-gray-500"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <h1 className="text-right">
-              Description
-            <p className="text-xs"> *optional </p>
+          <div className="space-y-2">
+            <h1>
+              Description <span className="text-sm text-muted-foreground">(optional)</span>
             </h1>
             <Input
               id="description"
               defaultValue="Description"
               className="col-span-3 text-gray-500"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
           </div>
+            <div className="space-y-2">
+              <p>Trip Duration <span className="text-sm text-muted-foreground">(optional)</span></p>
+              <DatePickerWithRange 
+                id="date_range"
+                className="col-span-3"
+                onDateChange={handleDateChange}
+                />
+            </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Create</Button>
+          <Button onClick={handleSubmit}type="submit">Create</Button>
         </DialogFooter>
-      </DialogContent>
-        </Dialog>
-    )
+    </DialogContent>
+      </Dialog>
+  )
 
 }
--- frontend/src/app/trips/page.tsx
+++ frontend/src/app/trips/page.tsx
@@ -5,47 +5,28 @@ import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
 import { Trip } from '../types';
 import { TripCard } from './components/card';
-import { Spacer } from '@heroui/react';
 import { CreateTripCard } from './components/create-trip-card';
 
 export default function Trips() {
 
-    const [title, setTitle] = React.useState('');
     const { getToken } = useAuth();
+    const [trips, setTrips] = React.useState([]);
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        const token = await getToken()
+    const fetchTrips = async () => {
+        const token = await getToken();
         try {
-            console.log(title)
             const headers = {
                 Authorization: `Bearer ${token}`,
                 'Content-Type': 'application/json'
             }
-            const { data } = await axios.post('http://localhost:8000/api/trips', { "title": title }, { headers });
-            console.log(data);
-            setTitle('');
+            const { data } = await axios.get('http://localhost:8000/api/trips', { headers });
+            setTrips(data);
         } catch (error) {
             console.error(error);
         }
-    }
+    };
 
-    const [trips, setTrips] = React.useState([]);
     React.useEffect(() => {
-        const fetchTrips = async () => {
-            const token = await getToken();
-            try {
-                const headers = {
-                    Authorization: `Bearer ${token}`,
-                    'Content-Type': 'application/json'
-                }
-                const { data } = await axios.get('http://localhost:8000/api/trips', { headers });
-                setTrips(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
         fetchTrips();
     }, [getToken]);
 
@@ -53,33 +34,14 @@ export default function Trips() {
         <div className='App flex h-screen'>
             <div className="w-full max-w-2xl mx-auto mt-10">
                 <h1 className="text-3xl font-bold text-gray-300 mb-6">Create a Trip</h1>
-                <form onSubmit={handleSubmit} className="space-y-6">
-                    <div>
-                        <label htmlFor="title" className="block text-sm font-medium text-gray-200">
-                            Title
-                        </label>
-                        <input
-                            type="text"
-                            id="title"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            required
-                            className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-                            placeholder="Enter trip title"
-                        />
-                    </div>
-                    <button type="submit" className="w-full px-3 py-2 mt-1 bg-indigo-500 text-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
-                        Submit
-                    </button>
-                </form>
                 <div>
                     <TripCard trip={{ trip_id: 1, owner_id: "1", title: 'Trip 1', description: 'Trip 1 description', start_date: '2021-10-01', end_date: '2021-10-10', created_at: '2021-09-01', updated_at: '2021-09-01' }}>
                     </TripCard>
                 </div>
                 <h2 className="text-2xl font-bold text-gray-300 mb-4">Trips</h2>
                 <div className="grid grid-cols-2 gap-4">
-                    <CreateTripCard></CreateTripCard>
-                    {trips.map((trip: Trip) => (
+                    <CreateTripCard onTripCreated={fetchTrips}></CreateTripCard>
+                    {trips.slice().reverse().map((trip: Trip) => (
                         <div key={trip.trip_id}>
                             <TripCard trip={trip} />
                         </div>
